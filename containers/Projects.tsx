724b149f71221c65fbec9615d46f4d7cfd06b706
Fatlum Sulaj <fatlumsulaj@gmail.com>
2023-11-21 00:48:51 +0100

    services done + projects half

--- containers/Projects.tsx
+++ containers/Projects.tsx
@@ -1,9 +1,103 @@
-import React from 'react'
+import React from "react";
+import Link from "next/link";
+import { BarChart4 } from "lucide-react";
+import { nunito } from "@/app/ui/fonts";
+import { ExternalLink } from "lucide-react";
+type ProjectStats = {
+  link: string;
+  text: string;
+  title: string;
+  linkText: string;
+  bgColor: string;
+  children: React.ReactNode;
+};
+
+const Project = ({ link, linkText, text, title, bgColor, children }: ProjectStats) => {
+  const dynamicStyles = {
+    backgroundColor: bgColor || 'transparent',
+  };
+
+  return (
+    <div className={`max-w-[400px] h-[280px] py-10 px-2 text-center text-white flex flex-col justify-evenly rounded-xl shadow-md`} style={dynamicStyles}>
+      <div className="flex flex-row justify-center items-center gap-1">
+        {children}
+        <h4 className="heading4">{title}</h4>
+      </div>
+      <p className="paragraph-sm">{text}</p>
+      <div className="flex flex-row justify-center items-center gap-1">
+      <ExternalLink/><Link href={link}>{linkText}</Link>
+      </div>
+    </div>
+  );
+};
+
 
 const Projects = () => {
   return (
-    <div>Projects</div>
-  )
-}
+    <section className={`max-container padding-container ${nunito.className} text-center my-24`}>
+      <h2 className="heading2">Meine aktuellen Arbeiten</h2>
+      <p className="paragraph">
+        Hier sind ein paar Designprojekte, an denen ich in der Vergangenheit
+        gearbeitet habe. Möchten Sie mehr sehen? Schreiben Sie mir eine E-Mail.
+      </p>
+      <div className="flex flex-wrap justify-center flex-row gap-6 mt-12">
+        <Project
+          link="/about"
+          text="Website für ein Digital-Marketing Unternehmen - Online Strategien revolutionieren"
+          title="DM Agency"
+          linkText="digital-marketing"
+          bgColor="#213f71"
+        >
+          <BarChart4 />
+        </Project>
+        <Project
+          link="/about"
+          text="Website für ein Digital-Marketing Unternehmen - Online Strategien revolutionieren"
+          title="DM Agency"
+          linkText="digital-marketing"
+          bgColor="#1aa6ab"
+        >
+          <BarChart4 />
+        </Project>
+        <Project
+          link="/about"
+          text="Website für ein Digital-Marketing Unternehmen - Online Strategien revolutionieren"
+          title="DM Agency"
+          linkText="digital-marketing"
+          bgColor="#4867d8"
+        >
+          <BarChart4 />
+        </Project>
+        <Project
+          link="/about"
+          text="Website für ein Digital-Marketing Unternehmen - Online Strategien revolutionieren"
+          title="DM Agency"
+          linkText="digital-marketing"
+          bgColor="#36393d"
+        >
+          <BarChart4 />
+        </Project>
+        <Project
+          link="/about"
+          text="Website für ein Digital-Marketing Unternehmen - Online Strategien revolutionieren"
+          title="DM Agency"
+          linkText="digital-marketing"
+          bgColor="#cb5c49"
+        >
+          <BarChart4 />
+        </Project>
+        <Project
+          link="/about"
+          text="Website für ein Digital-Marketing Unternehmen - Online Strategien revolutionieren"
+          title="DM Agency"
+          linkText="digital-marketing"
+          bgColor="#286c45"
+        >
+          <BarChart4 />
+        </Project>
+      </div>
+    </section>
+  );
+};
 
-export default Projects
+export default Projects;
--- containers/Skills.tsx
+++ containers/Skills.tsx
@@ -1,12 +1,14 @@
 import React from "react";
 import Image from "next/image";
+import { nunito } from "@/app/ui/fonts";
+import Link from "next/link";
 type SkillProps = {
   children: React.ReactNode;
 };
 
 const Skill = ({ children }: SkillProps) => {
   return (
-    <div className="w-full sm:w-[479px] h-[250px] border-[1px] border-gray-400 border-opacity-50 2xl:border-none rounded-xl">
+    <div className="w-full sm:max-w-[479px] h-auto border-[1px] border-gray-400 border-opacity-50 2xl:border-none rounded-xl">
       {children}
     </div>
   );
@@ -14,9 +16,11 @@ const Skill = ({ children }: SkillProps) => {
 
 const Skills = () => {
   return (
-    <section className="max-container container-padding flex flex-wrap justify-center border-0 2xl:border-[1px] border-gray-400 border-opacity-50 rounded-2xl mt-[-140px] bg-white">
+    <section
+      className={`max-container container-padding flex flex-wrap justify-center border-0 2xl:border-[1px] border-gray-300 border-opacity-50 rounded-2xl mt-[-140px] shadow-md bg-white ${nunito.className}`}
+    >
       <Skill>
-        <div className="flex flex-row items-center justify-center green-background p-5 rounded-full w-[75px] h-[75px] mx-auto mt-20">
+        <div className="flex flex-row items-center justify-center green-background p-5 rounded-full w-[75px] h-[75px] mx-auto mt-10 lg:mt-24">
           <Image
             src="/laptop-code-solid.svg"
             alt="web development"
@@ -24,10 +28,36 @@ const Skills = () => {
             height={50}
           />
         </div>
-        <h3 className="heading3 text-center">Web Developer</h3>
+        <div className="px-12 pb-10 lg:pb-24 text-center">
+          <h3 className="heading3 text-center py-4">Web Entwickler</h3>
+          <p className="paragraph-sm">
+            Ich liebe es, von Grund auf zu programmieren und Ideen im Browser
+            lebendig werden zu lassen.
+          </p>
+          <p className="paragraph-sm purple-text font-semibold pt-12 pb-1">
+            Programmiersprachen, die ich beherrsche:
+          </p>
+          <p className="paragraph-sm">
+            HTML, CSS, Tailwind, SASS, Bootstrap, Javasript, React, Next JS,
+            Typesript
+          </p>
+          <p className="paragraph-sm purple-text font-semibold pt-12 pb-1">
+            Dev Tools
+          </p>
+          <p className="paragraph-sm">Github</p>
+          <p className="paragraph-sm">Netlify</p>
+          <p className="paragraph-sm">Vercel</p>
+          <p className="paragraph-sm">Joomla</p>
+          <br />
+          <div className="mt-10">
+            <Link className="button-white" href="/">
+              Mehr Lesen
+            </Link>
+          </div>
+        </div>
       </Skill>
       <Skill>
-        <div className="w-full h-full border-0 2xl:border-x-[1px] lg:border-gray-400 lg:border-opacity-50 pt-20">
+        <div className="w-full h-full border-0 2xl:border-x-[1px] lg:border-gray-300 lg:border-opacity-50 pt-10 lg:pt-24">
           <div className="flex flex-row items-center justify-center green-background p-5 rounded-full w-[75px] h-[75px] mx-auto">
             <Image
               src="/wand-magic-sparkles-solid.svg"
@@ -36,11 +66,36 @@ const Skills = () => {
               height={50}
             />
           </div>
-          <h3 className="heading3 text-center">Designer</h3>
+          <div className="px-12 pb-10 lg:pb-24 text-center">
+            <h3 className="heading3 text-center py-4">Designer</h3>
+            <p className="paragraph-sm ">
+              Eine klare Inhaltsstruktur, eine leicht verständliche Darstellung
+              und verständlicher Inhalt sind für mich unverzichtbar.
+            </p>
+            <p className="paragraph-sm purple-text font-semibold pt-12 pb-1">
+              Was ich gerne entwerfe:
+            </p>
+            <p className="paragraph-sm">
+              UX, UI, Websites, Logos, Werbungen für Soziale Netzwerke
+            </p>
+            <p className="paragraph-sm purple-text font-semibold pt-12 pb-1">
+              Design Tools
+            </p>
+            <p className="paragraph-sm">Figma</p>
+            <p className="paragraph-sm">Adobe Express</p>
+            <p className="paragraph-sm">Font Awesome</p>
+            <p className="paragraph-sm">Pen & Paper</p>
+            <br />
+            <div className="mt-10">
+              <Link className="button-white" href="/">
+                Mehr Lesen
+              </Link>
+            </div>
+          </div>
         </div>
       </Skill>
       <Skill>
-        <div className="flex flex-row items-center justify-center green-background p-5 rounded-full w-[75px] h-[75px] mx-auto mt-20">
+        <div className="flex flex-row items-center justify-center green-background p-5 rounded-full w-[75px] h-[75px] mx-auto mt-10 lg:mt-24">
           <Image
             src="/magnifying-glass-chart-solid.svg"
             alt="Suchmaschinen Optimierung"
@@ -48,7 +103,33 @@ const Skills = () => {
             height={50}
           />
         </div>
-        <h3 className="heading3 text-center">SEO Expert</h3>
+        <div className="px-12 pb-10 lg:pb-24 text-center">
+          <h3 className="heading3 text-center py-4">SEO Expert</h3>
+          <p className="paragraph-sm">
+            Durch eine gezielte SEO-Strategie kann ich sicherstellen, dass meine
+            Webseites von den richtigen Nutzern gefunden und besucht wird.
+          </p>
+          <p className="paragraph-sm purple-text font-semibold pt-12 pb-1">
+            Ich bin hauptsächlich konzentriert auf:
+          </p>
+          <p className="paragraph-sm">
+            Content Creation, Werbungen, Keywords, Page Ranking, Link Building
+            usw.
+          </p>
+          <p className="paragraph-sm purple-text font-semibold pt-12 pb-1">
+            SEO Tools
+          </p>
+          <p className="paragraph-sm">Google Analytics</p>
+          <p className="paragraph-sm">Google Search Console</p>
+          <p className="paragraph-sm">Google Keyword Planner</p>
+          <p className="paragraph-sm">Semrush</p>
+          <p className="paragraph-sm">Ahrefs</p>
+          <div className="mt-10">
+            <Link className="button-white" href="/">
+              Mehr Lesen
+            </Link>
+          </div>
+        </div>
       </Skill>
     </section>
   );
